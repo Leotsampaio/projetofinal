390a13f89139e5fdcb39db29f35ca132d86a26de
joao victor <joao.araruna8@gmail.com>
2024-06-11 22:55:58 -0300

    nav e home

---
+++ src/app/_components/nav.tsx
@@ -0,0 +1,19 @@
+import styles from "~/styles/nav.module.css";
+
+export function Nav() {
+    return (
+        <nav className={styles.navBar}>
+            <div className={styles.navLogo}>
+                <img src="/images/logopf-removebg-preview.png" alt="imglogo" />
+            </div>
+            <div className={styles.navRight}>
+                <ul className={styles.navLinks}>
+                    <li><a href="#inicio">Início</a></li>
+                    <li><a href="#funcionalidades">Funcionalidades</a></li>
+                    <li><a href="#contato">Fale conosco</a></li>
+                </ul>
+                <button className={styles.btnLogin}>Login</button>
+            </div>
+        </nav>
+    );
+}
