78af66b3996a13ebddd1f1a658290c284afecd9e
leotsampaio <leotsampaio@gmail.com>
2024-06-20 16:12:53 -0300

    Alguns ajustes no footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -3,18 +3,29 @@ import styles from "~/styles/footer.module.css";
 export function Footer(){
     return(
         <nav className={styles.footer}>
-            <div className={styles.sobre}>
-                <ul className={styles.links}>
-                <li><strong>Mais informações</strong></li>
-                    <li><a href="https://www.instagram.com/structunb/">Instagram</a></li>
-                    <li><a href="https://struct.unb.br/">Site struct</a></li>
-                    <li><a>Ajuda</a></li>
-
-                </ul>
-            </div>
             <div className={styles.easyStockFooter}>
                 <h1>easyStock</h1>
             </div>
+            <div className={styles.sobre}>
+                <ul className={styles.links}>
+                <li><strong>Funcionalidades</strong></li>
+                    <li><a href="https://www.instagram.com/structunb/">fun.1</a></li>
+                    <li><a href="https://struct.unb.br/">fun.2</a></li>
+                    <li><a>fun.3</a></li>
+
+                </ul>
+            </div>
+            <div className={styles.sobre}>
+                <ul className={styles.links}>
+                <li><strong>Mais informações</strong></li>
+                    <li><a href="https://www.instagram.com/structunb/">Instagram</a><img src="/images/Instagram-Icon.png"/></li>
+                    <li><a href="https://www.facebook.com/StructUnB/?locale=pt_BR">Facebook</a><img src="/images/Facebook_Logo_2023.png"/></li>
+                    <li><a href="https://www.youtube.com/@structunb">Youtube</a><img src="/images/1384060.png"/></li>
+
+                </ul>
+            </div>
+            
+            
         </nav>
 
 
