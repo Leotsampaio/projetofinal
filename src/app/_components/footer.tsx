cc13cc789e26fd94c472797d0b4721225e2df4f0
leotsampaio <leotsampaio@gmail.com>
2024-06-21 16:06:20 -0300

    footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -8,16 +8,16 @@ export function Footer(){
             </div>
             <div className={styles.sobre}>
                 <ul className={styles.links}>
-                <li><strong>Funcionalidades</strong></li>
-                    <li><a href="https://www.instagram.com/structunb/">fun.1</a></li>
-                    <li><a href="https://struct.unb.br/">fun.2</a></li>
-                    <li><a>fun.3</a></li>
+                <li><strong>Contatos</strong></li>
+                    <li>EasyStock@unb.br.com</li>
+                    <li>(61)9 8765-12345</li>
+                    
 
                 </ul>
             </div>
             <div className={styles.sobre}>
                 <ul className={styles.links}>
-                <li><strong>Mais informações</strong></li>
+                <li><strong>Redes Sociais</strong></li>
                     <li><a href="https://www.instagram.com/structunb/">Instagram</a><img src="/images/Instagram-Icon.png"/></li>
                     <li><a href="https://www.facebook.com/StructUnB/?locale=pt_BR">Facebook</a><img src="/images/Facebook_Logo_2023.png"/></li>
                     <li><a href="https://www.youtube.com/@structunb">Youtube</a><img src="/images/1384060.png"/></li>
